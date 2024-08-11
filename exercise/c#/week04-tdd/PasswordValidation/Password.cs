d9ce0350d67d79a951e451f90ee38330890dd4a6
ROELLINGER <jonathan.roellinger@arteliagroup.com>
2024-08-11 15:20:09 +0200

    refactor: Week4-Tdd

--- exercise/c#/week04-tdd/PasswordValidation/Password.cs
+++ exercise/c#/week04-tdd/PasswordValidation/Password.cs
@@ -6,18 +6,17 @@ public sealed class Password
     public const string UpperCaseLetterError = "Password must contain at least one uppercase letter.";
     public const string LowerCaseLetterError = "Password must contain at least one lowercase letter.";
     public const string DigitError = "Password must contain at least one digit.";
-    public const string SpecialCharacterError = "Password must contain at least one of the following special characters: ., *, #, @, $, %, &";
     public const string InvalidCharacterError = "Password contains invalid special characters.";
-
-    private Password()
-    {
-    }
+    private const int MinLenghtValue = 8;
+    private static readonly List<char> _specialCharacters = new() { '.', '*', '#', '@', '$', '%', '&', };
+    public static readonly string SpecialCharacterError =
+        $"Password must contain at least one of the following special characters: {string.Join(", ", _specialCharacters)}";
 
     public static Result<Password> CreatePassword(string value)
     {
         IEnumerable<string> errors = ValidatePassword(value,
                                                       v => !string.IsNullOrWhiteSpace(v) ? null : EmptyPasswordError,
-                                                      v => v.Length >= 8 ? null : MinLengthError,
+                                                      v => HasMinLenght(v) ? null : MinLengthError,
                                                       v => HasUpperCaseLetter(v) ? null : UpperCaseLetterError,
                                                       v => HasLowerCaseLetter(v) ? null : LowerCaseLetterError,
                                                       v => HasDigit(v) ? null : DigitError,
@@ -30,11 +29,13 @@ public sealed class Password
 
     private static bool HasDigit(string value) => value.Any(char.IsDigit);
 
-    private static bool HasInvalidCharacter(string value) => value.Any(c => !".*#@$%&".Contains(c) && !char.IsLetterOrDigit(c));
+    private static bool HasInvalidCharacter(string value) => value.Any(c => !_specialCharacters.Contains(c) && !char.IsLetterOrDigit(c));
 
     private static bool HasLowerCaseLetter(string value) => value.Any(char.IsLower);
 
-    private static bool HasSpecialCharacter(string value) => value.Any(c => ".*#@$%&".Contains(c));
+    private static bool HasMinLenght(string value) => value.Length >= MinLenghtValue;
+
+    private static bool HasSpecialCharacter(string value) => value.Any(_specialCharacters.Contains);
 
     private static bool HasUpperCaseLetter(string value) => value.Any(char.IsUpper);
 
